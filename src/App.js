33615f2d8bbdb4eb63753cfae191e90ac5529716
roji <roji@onepointfinserv.com>
2024-05-28 16:11:22 +0545

    updates 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import './App.css';
-import About from './components/About';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Projects from './containers/Projects';
